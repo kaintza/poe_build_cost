e2cd5dfff1cfda4764794f07f764ac9f8e7e8606
Adam <adam.kaintz@verycreatives.com>
2020-06-14 19:18:48 +0200

    Add Schema base

---
+++ lib/poe_build_cost_schema/base.ex
@@ -0,0 +1,15 @@
+defmodule PoeBuildCostSchema.Base do
+  def schema do
+    quote do
+      use Ecto.Schema
+
+      import Ecto
+      import Ecto.Changeset
+      import Ecto.Query
+    end
+  end
+
+  defmacro __using__(which) when is_atom(which) do
+    apply(__MODULE__, which, [])
+  end
+end
