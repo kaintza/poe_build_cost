add36437a1fb7f394ddf4f020b1eafd1e917fcd4
Adam <adam.kaintz@verycreatives.com>
2020-06-14 19:20:08 +0200

    WIP build live render item liveview components

--- lib/poe_build_cost_web/live/build_live.ex
+++ lib/poe_build_cost_web/live/build_live.ex
@@ -1,11 +1,21 @@
 defmodule PoeBuildCostWeb.BuildLive do
-
   use PoeBuildCostWeb, :live_view
+  alias PoeBuildCostWeb.LiveComponent.ItemComponent
 
   @impl true
-  def mount(_params, _session, socket) do
-    {:ok, assign(socket, show_modal: false, items: %{})}
+  def mount(%{"build_url" => build_url}, _session, socket) do
+    build = PoeBuildCostSchema.Build |> PoeBuildCost.Repo.get_by!(url_prefix: build_url) |> PoeBuildCost.Repo.preload(:items)
+    {:ok, assign(socket, show_modal: false, slots: slots(), build: build)}
   end
+
+  def mount(_params, _session, socket) do
+    {:ok, assign(socket, show_modal: false, slots: slots(), build: nil)}
+  end
+
+  def slots do
+    [:helmet, :chest, :belt, :amulet, :weapon_left, :weapon_right, :ring_left, :ring_right, :gloves, :boots, :flask1, :flask2, :flask3, :flask4, :flask5]
+  end
+
 
   def handle_params(%{id: id}, socket) do
   end
